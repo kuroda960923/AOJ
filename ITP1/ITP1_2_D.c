9869f07cafd31d5d80559b483aee33c0fdae6716
kuroda960923 <kuroda960923@gmail.com/>
2021-01-05 01:15:06 +0900

    change file

--- ITP1/ITP1_2_D.c
+++ ITP1/ITP1_2_D.c
@@ -3,21 +3,14 @@
 int main(){
     int W, H, x, y, r = 0;
     scanf("%d %d %d %d %d", &W, &H, &x, &y, &r);
-    if(r - x >= 0 && r + x <= W){
-        if(r - y >= 0 && r + y <= H){
-            printf("")
+    if(x - r >= 0 && x + r <= W){
+        if(y - r >= 0 && y + r <= H){
+            printf("Yes\n");
+        }else{
+            printf("No\n");
         }
+    }else{
+        printf("No\n");
     }
-    if(b > c){
-        temp = b, b = c, c = temp;
-    }
-    if(a > b){
-        temp = a, a = b, b = temp;
-    }
-    if(b > c){
-        temp = b, b = c, c = temp;
-    }
-    printf("%d %d %d\n",a, b, c);
-
     return 0;
 }
